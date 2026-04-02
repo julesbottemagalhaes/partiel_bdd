6a770d74e1d0cc02ac29e106b2e68847da24dbcb
Jules Botté-Magalhaes <144135572+julesbottemagalhaes@users.noreply.github.com>
2026-04-02 11:49:03 +0200

    Fix trigger to validate note values correctly

--- Question5.sql
+++ Question5.sql
@@ -2,12 +2,12 @@ CREATE TRIGGER Verifier_note
 BEFORE INSERT ON note
 FOR EACH ROW
 BEGIN
-    DECLARE max_note FLOAT;
-    DECLARE min_note FLOAT;
-IF max_note IS NOT NULL AND NEW.valeur > 100 THEN
+# je me suis aperçu en testant avec cette commande : INSERT INTO note (id_run, id_juge, id_critere, valeur) VALUES (15,6,4,101);
+# que mon trigger ne marchait pas donc j'ai essayé avec cette autre méthode et apres les test cela marche
+    IF NEW.valeur > 100 THEN 
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'erreur, la note ne peut pas être au dessus de 100';
     END IF;
-IF min_note IS NOT NULL AND NEW.valeur < 0 THEN
+    IF NEW.valeur < 0 THEN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'erreur, la note ne peut pas être en dessous de 0';
     END IF;
 END;
