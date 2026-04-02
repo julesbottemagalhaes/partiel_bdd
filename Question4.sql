e603b4c77941e9c70a7fb32e40df5fd46c919da4
Jules Botté-Magalhaes <144135572+julesbottemagalhaes@users.noreply.github.com>
2026-04-02 11:33:05 +0200

    Create stored procedure for event ranking

--- Question4.sql
+++ Question4.sql
@@ -1,1 +1,8 @@
-Question4.sql
+CREATE PROCEDURE Classement_par_epreuve()
+BEGIN
+    SELECT epreuve.nom, athlete.nom, athlete.prenom, AVG(note.valeur)
+    FROM epreuve
+    INNER JOIN participation ON epreuve.id_epreuve = participation.id_epreuve
+    INNER JOIN athlete ON participation.id_athlete = athlete.id_athlete
+    INNER JOIN note ON participation.id_participation = note.id_participation
+END;
