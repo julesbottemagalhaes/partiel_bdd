20d584df63d98f8e17a4541c7788662f52a95666
Jules Botté-Magalhaes <144135572+julesbottemagalhaes@users.noreply.github.com>
2026-04-02 11:15:11 +0200

    Add count of events per athlete in query

--- Question2.sql
+++ Question2.sql
@@ -1,3 +1,4 @@
-SELECT athlete.nom, athlete.prenom FROM athlete
+SELECT athlete.nom, athlete.prenom, COUNT(*) AS nombre_epreuves FROM athlete
 INNER JOIN participation ON athlete.id_athlete = participation.id_athlete
 INNER JOIN epreuve ON participation.id_epreuve = epreuve.id_epreuve
+GROUP BY athlete.id_athlete
