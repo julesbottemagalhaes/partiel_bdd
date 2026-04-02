304e61b76ecd5cf3f9c77a15eba3f610473f0153
Jules Botté-Magalhaes <144135572+julesbottemagalhaes@users.noreply.github.com>
2026-04-02 11:39:17 +0200

    Add validation to prevent notes above 100

--- Question5.sql
+++ Question5.sql
@@ -4,4 +4,7 @@ FOR EACH ROW
 BEGIN
     DECLARE max_note FLOAT;
     DECLARE min_note FLOAT;
+IF max_note IS NOT NULL AND NEW.valeur > 100 THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'erreur, la note ne peut pas être au dessus de 100';
+    END IF;
 END;
