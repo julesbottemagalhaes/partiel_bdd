0ed7f66afbe7ca197d4311d29df662d043e7ba79
Jules Botté-Magalhaes <144135572+julesbottemagalhaes@users.noreply.github.com>
2026-04-02 11:11:20 +0200

    Calculate average score for athletes by event

--- Question1.sql
+++ Question1.sql
@@ -1,5 +1,6 @@
-SELECT athlete.id_athlete, athlete.nom, athlete.prenom, epreuve.nom, note.valeur FROM athlete
+SELECT athlete.id_athlete, athlete.nom, athlete.prenom, epreuve.nom, AVG(note.valeur) FROM athlete
 INNER JOIN participation ON athlete.id_athlete = participation.id_athlete
 INNER JOIN epreuve ON participation.id_epreuve = epreuve.id_epreuve
 INNER JOIN run ON epreuve.id_epreuve = run.id_epreuve
 INNER JOIN note ON run.id_run = note.id_run
+GROUP BY athlete.id_athlete, epreuve.id_epreuve
