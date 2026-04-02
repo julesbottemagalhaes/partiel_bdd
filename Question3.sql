3e9cd6e0aa8deae2a6be715b9827314a2bb2fda7
Jules Botté-Magalhaes <144135572+julesbottemagalhaes@users.noreply.github.com>
2026-04-02 11:19:37 +0200

    Add joins to include participation and run data

--- Question3.sql
+++ Question3.sql
@@ -1,2 +1,5 @@
 SELECT athlete.nom, pays.nom FROM athlete
 INNER JOIN pays ON athlete.id_pays = pays.id_pays
+INNER JOIN participation ON athlete.id_athlete = participation.id_athlete
+INNER JOIN epreuve ON participation.id_epreuve = epreuve.id_epreuve
+INNER JOIN run ON epreuve.id_epreuve = run.id_epreuve
