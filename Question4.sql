b046c8b51d63d6febe077cc8fa9b98940f1d5628
Jules Botté-Magalhaes <144135572+julesbottemagalhaes@users.noreply.github.com>
2026-04-02 11:35:18 +0200

    Enhance SQL query with grouping and ordering
    Added GROUP BY and ORDER BY clauses to aggregate and sort results.

--- Question4.sql
+++ Question4.sql
@@ -5,4 +5,6 @@ BEGIN
     INNER JOIN participation ON epreuve.id_epreuve = participation.id_epreuve
     INNER JOIN athlete ON participation.id_athlete = athlete.id_athlete
     INNER JOIN note ON participation.id_participation = note.id_participation
+    GROUP BY epreuve.id_epreuve, athlete.id_athlete
+    ORDER BY AVG(note.valeur) DESC;
 END;
