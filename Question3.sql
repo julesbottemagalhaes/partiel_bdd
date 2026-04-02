98d828bc4abaa195f8bc501f538258d6281d4e47
Jules Botté-Magalhaes <144135572+julesbottemagalhaes@users.noreply.github.com>
2026-04-02 11:28:17 +0200

    Add condition to filter runs with no associated event

--- Question3.sql
+++ Question3.sql
@@ -3,3 +3,4 @@ INNER JOIN pays ON athlete.id_pays = pays.id_pays
 INNER JOIN participation ON athlete.id_athlete = participation.id_athlete
 INNER JOIN epreuve ON participation.id_epreuve = epreuve.id_epreuve
 INNER JOIN run ON epreuve.id_epreuve = run.id_epreuve
+WHERE run.id_epreuve IS NULL
