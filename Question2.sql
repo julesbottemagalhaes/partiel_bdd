b0b60f6bdde41415f482c4beef80157c8cc152f7
Jules Botté-Magalhaes <144135572+julesbottemagalhaes@users.noreply.github.com>
2026-04-02 11:16:04 +0200

    Add HAVING clause to filter athletes with 2+ events

--- Question2.sql
+++ Question2.sql
@@ -2,3 +2,4 @@ SELECT athlete.nom, athlete.prenom, COUNT(*) AS nombre_epreuves FROM athlete
 INNER JOIN participation ON athlete.id_athlete = participation.id_athlete
 INNER JOIN epreuve ON participation.id_epreuve = epreuve.id_epreuve
 GROUP BY athlete.id_athlete
+HAVING nombre_epreuves >= 2
